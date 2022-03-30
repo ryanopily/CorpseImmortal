e00c3b6a254751aea3fbf68818adcb93782efdb8
Nicholas Eliyesil <neliyesil2626@westfield.ma.edu>
2022-03-29 21:17:50 -0400

    changed to leftClickEvent()

--- src/main/java/ml/zer0dasho/corpseimmortal/events/CorpseClickListener.java
+++ src/main/java/ml/zer0dasho/corpseimmortal/events/CorpseClickListener.java
@@ -1,12 +1,13 @@
 package ml.zer0dasho.corpseimmortal.events;
 
 import net.citizensnpcs.api.event.NPCClickEvent;
+import net.citizensnpcs.api.event.NPCLeftClickEvent;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 
 public class CorpseClickListener implements Listener {
   @EventHandler
-  public void openInventory(NPCClickEvent e){
+  public void openInventory(NPCLeftClickEvent e){
     e.getClicker().sendMessage(e.getNPC().getName());
   }
 }
